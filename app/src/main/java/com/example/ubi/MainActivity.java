785f145c09627acbabb4b45acfac2e5fd2577113
takatan <takatan@ubi.cs.ritsumei.ac.jp>
2019-09-05 11:41:36 +0900

    センサデータをcsvで保存

--- app/src/main/java/com/example/ubi/MainActivity.java
+++ app/src/main/java/com/example/ubi/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.ubi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.hardware.Sensor;
@@ -9,11 +14,24 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.List;
+
+import static android.provider.Telephony.Mms.Part.FILENAME;
+
 public class MainActivity extends Activity implements Runnable, SensorEventListener {
     SensorManager sm;
     TextView tv;
@@ -32,6 +50,8 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
 
         h = new Handler();
         h.postDelayed(this, 500);
+
+
     }
 
     @Override
@@ -73,9 +93,51 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
         gy = event.values[1];
         gz = event.values[2];
         a = (float)Math.sqrt(gx*gx+gy*gy+gz*gz);
+
+        String filename = "kasokusenser.csv";
+        String output = String.valueOf(gx)+","
+                +String.valueOf(gy)+","
+                +String.valueOf(gz)+","
+                +String.valueOf(a)+"\n";
+
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(filename,MODE_APPEND);
+            outputStream.write(output.getBytes());
+            outputStream.close();
+
+            //終了メッセージを画面に出力する
+            Log.v("test","出力が完了しました。");
+
+        } catch (Exception e) {
+            //例外時処理
+            Log.v("test","出力に失敗しました。");
+            e.printStackTrace();
+
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    private static final int REQUEST_EXTERNAL_STORAGE_CODE = 0x01;
+    private static String[] mPermissions = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    };
+
+    private static void verifyStoragePermissions(Activity activity) {
+        int readPermission = ContextCompat.checkSelfPermission(activity, mPermissions[0]);
+        int writePermission = ContextCompat.checkSelfPermission(activity, mPermissions[1]);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED ||
+                readPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    activity,
+                    mPermissions,
+                    REQUEST_EXTERNAL_STORAGE_CODE
+            );
+        }
     }
 }
