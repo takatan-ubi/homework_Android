ea4d861734d9c23c61628777fb0b17283d26388f
takatan <takatan@ubi.cs.ritsumei.ac.jp>
2019-09-06 18:05:37 +0900

    Wekaで分類機の作成まで完了 分類結果が一度しか表示されない

--- app/src/main/java/com/example/ubi/MainActivity.java
+++ app/src/main/java/com/example/ubi/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.ubi;
 
 import android.Manifest;
-import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.hardware.Sensor;
@@ -12,7 +11,6 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -22,18 +20,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.sql.DataSource;
 
 import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
 import weka.classifiers.functions.SMO;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.FastVector;
+import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.converters.ConverterUtils;
+import weka.core.converters.ConverterUtils.DataSource;
 
 public class MainActivity extends Activity implements Runnable, SensorEventListener {
     SensorManager sm;
@@ -44,30 +45,37 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
     RadioButton radioButton;
 
     Handler h;
-    double gx, gy, gz, a;
+    double gx, gy, gz, acceleration;
     boolean isRecording = false;
+    int endRecordNum = 0;
     String filename = "Standing.csv";
     String state ="Standing";
     List<String> stateList = Arrays.asList("Standing","Walking","Running");
+    TextView stateText;
+
+    Evaluation eval;
+    Instances instances;
+    Classifier classifier;
+    Attribute a;
+    boolean isLearned = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        LinearLayout ll = new LinearLayout(this);
         setContentView(R.layout.activity_main);
 
         h = new Handler();
         h.postDelayed(this, 500);
 
         // 加速度データの表示テキストウィンド
-        tv = (TextView)findViewById(R.id.SenserDataText) ;
+        tv = (TextView)findViewById(R.id.SenserDataText);
 
         // データ記録時の状態の選択をするラジオボタン
         radioGroup = (RadioGroup)findViewById(R.id.RadioGroup);
 
         // デバッグ用　状態表示
-        final TextView stateText = (TextView)findViewById(R.id.stateText);
+        stateText = (TextView)findViewById(R.id.stateText);
 
         //データ記録開始,停止ボタン
         recordStartButton = (Button)findViewById(R.id.recordStartButton);
@@ -78,6 +86,12 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
             @Override
             public void onClick(View view) {
                 if(!isRecording){
+//                    String path = "/data/data/com.example.ubi/"+radioButton.getText()+".csv";
+//                    File f =   new File(path);
+//                    if (f.exists()){
+//                        f.delete();
+//                        Log.v("test","file delete" + radioButton.getText());
+//                    }
                     stateText.setText("記録中");
                     isRecording = true;
                 }
@@ -90,47 +104,19 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
                 if(isRecording){
                     stateText.setText("待機中");
                     isRecording = false;
+                    endRecordNum++;
+                    if(endRecordNum == 3){
+                        changeCsvToArff();
+                        endRecordNum++;
+                    }
                 }
             }
         });
 
-        //学習データ保存用ファイルの作成
-//        String output =
-//                "@relation smartphoneZombie\n" +
-//                "\n" +
-//                "@attribute x numeric\n" +
-//                "@attribute y numeric\n" +
-//                "@attribute z numeric\n" +
-//                "@attribute a numeric\n" +
-//                "@attribute state {Standing, Walking, Running}\n" +
-//                "\n" +
-//                "@DATA\n";
-//        FileOutputStream outputStream;
-//        try {
-//            filename = "Standing.arff";
-//            outputStream = openFileOutput(filename, MODE_APPEND);
-//            outputStream.write(output.getBytes());
-//            File file = new File(filename);
-//            if (file.exists()){
-//                Log.v("test","ファイルは存在します");
-//                file.delete();
-//            }else{
-//                Log.v("test","ファイルは存在しません");
-//            }
-//            outputStream.close();
-//
-//            filename = "Walking.arff";
-//            outputStream = openFileOutput(filename, MODE_APPEND);
-//            outputStream.write(output.getBytes());
-//            outputStream.close();
-//
-//            filename = "Running.arff";
-//            outputStream = openFileOutput(filename, MODE_APPEND);
-//            outputStream.write(output.getBytes());
-//            outputStream.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        if(isLearned) {
+
+        }
+
     }
 
     public void checkRadioButton(View v){
@@ -138,23 +124,60 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
         radioButton = findViewById(radioId);
         filename = radioButton.getText()+".csv";
         state = radioButton.getText()+"";
+
     }
 
     public void startLearning(View v){
+        try {
+            stateText.setText("学習中");
+            //学習データの生成
+            FileInputStream inputStream = openFileInput("learnData.arff");
+            DataSource source = new DataSource(inputStream);
+            instances = source.getDataSet();
+            instances.setClassIndex(1);
+
+            //分類機の生成
+            classifier = new SMO();
+            classifier.buildClassifier(instances);
+            eval = new Evaluation(instances);
+            eval.evaluateModel(classifier, instances);      //モデルと学習データから評価
+            Log.v("test",eval.toSummaryString());
+            a = new Attribute("a", 0);
+            Instance instance = new DenseInstance(2) {
+            };
+            instance.setValue(a, acceleration);
+            instance.setDataset(instances);
+            double result = classifier.classifyInstance(instance);
+            stateText.setText(stateList.get((int) result));
+            isLearned = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.v("test","damedesita");
+        }
+
+    }
+
+    public void changeCsvToArff(){
         try {
             Log.v("test","startleanin");
 
             //学習データ保存用ファイルの作成
             String filetop =
                     "@relation smartphoneZombie\n" +
-                    "\n" +
-                    "@attribute x numeric\n" +
-                    "@attribute y numeric\n" +
-                    "@attribute z numeric\n" +
-                    "@attribute a numeric\n" +
-                    "@attribute state {Standing, Walking, Running}\n" +
-                    "\n" +
-                    "@DATA\n";
+                            "\n" +
+//                            "@attribute x numeric\n" +
+//                            "@attribute y numeric\n" +
+//                            "@attribute z numeric\n" +
+                            "@attribute a numeric\n" +
+                            "@attribute state {Standing, Walking, Running}\n" +
+                            "\n" +
+                            "@DATA\n";
+            File file = new File("/data/data/com.example.ubi/files/learnData.arff");
+            Log.v("test","ファイルを初期化したい");
+            if (file.exists()){
+                file.delete();
+                Log.v("test","ファイルの初期化");
+            }
             FileOutputStream learnoutputStream;
             learnoutputStream = openFileOutput("learnData.arff", MODE_APPEND);
             learnoutputStream.write(filetop.getBytes("UTF-8"));
@@ -166,11 +189,16 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
 
                 String line = "";
                 while ((line = bufferReader.readLine()) != null) {
-                    learnoutputStream.write(line.getBytes("UTF-8"));
+                    //カンマ区切りで１つづつ配列に入れる
+                    String[] rowData = line.split(",");
+                    //合成加速度
+                    String data = rowData[3] + ", " + rowData[4];
+                    learnoutputStream.write(data.getBytes("UTF-8"));
                     learnoutputStream.write("\n".getBytes("UTF-8"));
                 }
                 bufferReader.close();
                 Log.v("test",csvFileName+"finish");
+
             }
             learnoutputStream.close();
 
@@ -178,9 +206,6 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
             e.printStackTrace();
             Log.v("test","erroer");
         }
-
-
-
     }
 
     @Override
@@ -188,7 +213,8 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
         tv.setText("X-axis : " + gx + "\n"
                 + "Y-axis : " + gy + "\n"
                 + "Z-axis : " + gz + "\n"
-                + "XYZ    : " + a + "\n");
+                + "XYZ    : " + acceleration + "\n"
+                + "endNum :" + endRecordNum + "\n");
         h.postDelayed(this, 500);
     }
 
@@ -218,17 +244,22 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if(isRecording) {
+
             gx = event.values[0];
             gy = event.values[1];
             gz = event.values[2];
-            a = (float)Math.sqrt(gx*gx+gy*gy+gz*gz);
+            acceleration = (float)Math.sqrt(gx*gx+gy*gy+gz*gz);
+            recordSensor();
+    }
+
+    public void recordSensor(){
+        if(isRecording) {
 
             String output = BigDecimal.valueOf(gx).toPlainString() + ","
                     + BigDecimal.valueOf(gy).toPlainString() + ","
                     + BigDecimal.valueOf(gz).toPlainString() + ","
-                    + BigDecimal.valueOf(a).toPlainString() + ","
-                    + state +"\n";
+                    + BigDecimal.valueOf(acceleration).toPlainString() + ","
+                    + " "+state +"\n";
             FileOutputStream outputStream;
             try {
                 outputStream = openFileOutput(filename, MODE_APPEND);
@@ -238,7 +269,6 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
                 e.printStackTrace();
             }
         }
-
     }
 
     @Override
@@ -250,8 +280,4 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
     };
-
-
-
-
 }
