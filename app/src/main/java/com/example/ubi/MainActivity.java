830b372b8047013f881742f8c4031c66920892a1
takatan <takatan@ubi.cs.ritsumei.ac.jp>
2019-09-06 11:14:19 +0900

    csvファイルをarffファイルに変換 wekaSTRIPPED.jarはインポート済み

--- app/src/main/java/com/example/ubi/MainActivity.java
+++ app/src/main/java/com/example/ubi/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ubi;
 
 import android.Manifest;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.hardware.Sensor;
@@ -8,23 +9,46 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
+
+import javax.sql.DataSource;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.functions.SMO;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
 
 public class MainActivity extends Activity implements Runnable, SensorEventListener {
     SensorManager sm;
     TextView tv;
     Button recordStartButton,recordStopButton;
+
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+
     Handler h;
     double gx, gy, gz, a;
     boolean isRecording = false;
-    boolean flag = false;
+    String filename = "Standing.csv";
+    String state ="Standing";
+    List<String> stateList = Arrays.asList("Standing","Walking","Running");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +62,9 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
 
         // 加速度データの表示テキストウィンド
         tv = (TextView)findViewById(R.id.SenserDataText) ;
+
+        // データ記録時の状態の選択をするラジオボタン
+        radioGroup = (RadioGroup)findViewById(R.id.RadioGroup);
 
         // デバッグ用　状態表示
         final TextView stateText = (TextView)findViewById(R.id.stateText);
@@ -67,6 +94,90 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
             }
         });
 
+        //学習データ保存用ファイルの作成
+//        String output =
+//                "@relation smartphoneZombie\n" +
+//                "\n" +
+//                "@attribute x numeric\n" +
+//                "@attribute y numeric\n" +
+//                "@attribute z numeric\n" +
+//                "@attribute a numeric\n" +
+//                "@attribute state {Standing, Walking, Running}\n" +
+//                "\n" +
+//                "@DATA\n";
+//        FileOutputStream outputStream;
+//        try {
+//            filename = "Standing.arff";
+//            outputStream = openFileOutput(filename, MODE_APPEND);
+//            outputStream.write(output.getBytes());
+//            File file = new File(filename);
+//            if (file.exists()){
+//                Log.v("test","ファイルは存在します");
+//                file.delete();
+//            }else{
+//                Log.v("test","ファイルは存在しません");
+//            }
+//            outputStream.close();
+//
+//            filename = "Walking.arff";
+//            outputStream = openFileOutput(filename, MODE_APPEND);
+//            outputStream.write(output.getBytes());
+//            outputStream.close();
+//
+//            filename = "Running.arff";
+//            outputStream = openFileOutput(filename, MODE_APPEND);
+//            outputStream.write(output.getBytes());
+//            outputStream.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public void checkRadioButton(View v){
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+        filename = radioButton.getText()+".csv";
+        state = radioButton.getText()+"";
+    }
+
+    public void startLearning(View v){
+        try {
+            Log.v("test","startleanin");
+
+            //学習データ保存用ファイルの作成
+            String filetop =
+                    "@relation smartphoneZombie\n" +
+                    "\n" +
+                    "@attribute x numeric\n" +
+                    "@attribute y numeric\n" +
+                    "@attribute z numeric\n" +
+                    "@attribute a numeric\n" +
+                    "@attribute state {Standing, Walking, Running}\n" +
+                    "\n" +
+                    "@DATA\n";
+            FileOutputStream learnoutputStream;
+            learnoutputStream = openFileOutput("learnData.arff", MODE_APPEND);
+            learnoutputStream.write(filetop.getBytes("UTF-8"));
+
+            for(String csvFileName : stateList){
+                FileInputStream inputStream = openFileInput(csvFileName+".csv");
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferReader = new BufferedReader(inputStreamReader);
+
+                String line = "";
+                while ((line = bufferReader.readLine()) != null) {
+                    learnoutputStream.write(line.getBytes("UTF-8"));
+                    learnoutputStream.write("\n".getBytes("UTF-8"));
+                }
+                bufferReader.close();
+                Log.v("test",csvFileName+"finish");
+            }
+            learnoutputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.v("test","erroer");
+        }
 
 
 
@@ -107,21 +218,21 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        gx = event.values[0];
-        gy = event.values[1];
-        gz = event.values[2];
-        a = (float)Math.sqrt(gx*gx+gy*gy+gz*gz);
-
         if(isRecording) {
-            String filename = "kasokusenser.csv";
+            gx = event.values[0];
+            gy = event.values[1];
+            gz = event.values[2];
+            a = (float)Math.sqrt(gx*gx+gy*gy+gz*gz);
+
             String output = BigDecimal.valueOf(gx).toPlainString() + ","
                     + BigDecimal.valueOf(gy).toPlainString() + ","
                     + BigDecimal.valueOf(gz).toPlainString() + ","
-                    + BigDecimal.valueOf(a).toPlainString() + "\n";
+                    + BigDecimal.valueOf(a).toPlainString() + ","
+                    + state +"\n";
             FileOutputStream outputStream;
             try {
                 outputStream = openFileOutput(filename, MODE_APPEND);
-                outputStream.write(output.getBytes());
+                outputStream.write(output.getBytes("UTF-8"));
                 outputStream.close();
             } catch (Exception e) {
                 e.printStackTrace();
